6204835561682264dd978d893791b5f51365724d
annainirraref <73178733+annainirraref@users.noreply.github.com>
2021-03-16 15:52:27 +0100

    bootstrap added, component Weather

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+import Weather from "Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
