ea1eda3e4b9f647d5bcc5d11b4511a63f8a71090
annainirraref <73178733+annainirraref@users.noreply.github.com>
2021-03-18 15:54:32 +0100

    forecast component- trial

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Weather.css";
-
+import Forecast from "./Forecast.js";
 import yellowsun from "./images/yellowsun.png";
 
 export default function Weather() {
@@ -33,22 +33,25 @@ export default function Weather() {
       </div>
 
       <div className="main-card">
-        <h5 className="card-text">Weather description</h5>
         <div className="main-card card-body">
           <section className="second-card">
             <div className="second-card-body">
               <div className="row">
                 <div className="col-sm-8">
+                  <h5 className="card-text">Sunny</h5>
                   <img className="img-left" src={yellowsun} alt="Sun" />
                 </div>
 
-                <div className="row col-sm-4">
+                <div className="col-sm-4">
                   <div className="card third-card">
                     <div className="card-title third-card-title">
                       {" "}
-                      <span className="third-card-title today"> </span>
+                      <span className="third-card-title today">
+                        Today, 15 March 2021
+                      </span>
                       <br />{" "}
-                      <span className="third-card-title temperature"> </span> °
+                      <span className="third-card-title temperature"> 25 </span>{" "}
+                      °
                       <a href="#" className="celsius">
                         C
                       </a>
@@ -72,81 +75,99 @@ export default function Weather() {
                 </div>
               </div>
             </div>
-
-            <div className="card-deck">
-              <div className="card card-day1">
-                <div className="card-body">
-                  <h5 className="card-title">Monday</h5>
-                  <img
-                    className="img-day1"
-                    src="src/images/cloudy-sun.png"
-                    className="card-img-top"
-                    alt="cloudy"
-                  />
-                  <p className="card-text-days">
-                    ▲ 17° C <br /> ▼ 11° C
-                  </p>
-                </div>
-              </div>
-
-              <div className="card card-day2">
-                <div className="card-body">
-                  <h5 className="card-title">Tuesday </h5>
-                  <img
-                    src="src/images/rain.png"
-                    class="card-img-top img-day2"
-                    alt="rain"
-                  />
-                  <p className="card-text-days">
-                    ▲ 17° C <br /> ▼ 11° C
-                  </p>
-                </div>
-              </div>
-
-              <div className="card card-day3">
-                <div className="card-body">
-                  <h5 className="card-title">Wednesday</h5>
-                  <img
-                    src="src/images/storm.png"
-                    class="img-day3 card-img-top"
-                    alt="storm"
-                  />
-                  <p className="card-text-days">
-                    ▲ 17° C <br /> ▼ 11° C
-                  </p>
-                </div>
-              </div>
-
-              <div className="card card-day4">
-                <div className="card-body">
-                  <h5 className="card-title">Thursday</h5>
-                  <img
-                    src="src/images/cloudy-sun2.png"
-                    className="img-day4 card-img-top"
-                    alt="cloudy2"
-                  />
-                  <p className="card-text-days">
-                    ▲ 17° C <br /> ▼ 11° C
-                  </p>
-                </div>
-              </div>
-
-              <div className="card card-day5"> </div>
-              <div className="card-body">
-                <h5 className="card-title">Friday</h5>
-                <img
-                  src="src/images/fog.png"
-                  className="img-day5 card-img-top"
-                  alt="fog"
-                />
-                <p className="card-text-days">
-                  ▲ 17° C <br /> ▼ 11° C{" "}
-                </p>
-              </div>
-            </div>
           </section>
         </div>
       </div>
+
+      <div className="card-deck">
+        <div className="card card-day1">
+          <div className="card-body">
+            <h5 className="card-title">Monday</h5>
+            <img
+              className="img-day1"
+              src="src/images/cloudy-sun.png"
+              className="card-img-top"
+              alt="cloudy"
+            />
+            <p className="card-text-days">
+              ▲ 17° C <br /> ▼ 11° C
+            </p>
+          </div>
+        </div>
+
+        <div className="card card-day2">
+          <div className="card-body">
+            <h5 className="card-title">Tuesday </h5>
+            <img
+              src="src/images/rain.png"
+              class="card-img-top img-day2"
+              alt="rain"
+            />
+            <p className="card-text-days">
+              ▲ 17° C <br /> ▼ 11° C
+            </p>
+          </div>
+        </div>
+
+        <div className="card card-day3">
+          <div className="card-body">
+            <h5 className="card-title">Wednesday</h5>
+            <img
+              src="src/images/storm.png"
+              class="img-day3 card-img-top"
+              alt="storm"
+            />
+            <p className="card-text-days">
+              ▲ 17° C <br /> ▼ 11° C
+            </p>
+          </div>
+        </div>
+
+        <div className="card card-day4">
+          <div className="card-body">
+            <h5 className="card-title">Thursday</h5>
+            <img
+              src="src/images/cloudy-sun2.png"
+              className="img-day4 card-img-top"
+              alt="cloudy2"
+            />
+            <p className="card-text-days">
+              ▲ 17° C <br /> ▼ 11° C
+            </p>
+          </div>
+        </div>
+
+        <div className="card card-day5"> </div>
+        <div className="card-body">
+          <h5 className="card-title">Friday</h5>
+          <img
+            src="src/images/fog.png"
+            className="img-day5 card-img-top"
+            alt="fog"
+          />
+          <p className="card-text-days">
+            ▲ 17° C <br /> ▼ 11° C{" "}
+          </p>
+        </div>
+      </div>
+
+      <div class="card-deck">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Forecast />
     </div>
   );
 }
